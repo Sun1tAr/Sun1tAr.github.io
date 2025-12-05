5eb55c368b7fd030964e0996a7e1ddf7520dd9cd
Sun1tAr <denchik2376@yandex.ru>
2025-12-05 16:08:10 +0300

    Update ProjectsSection.js

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/AboutSection.css';
 
+
+
 function ProjectsSection() {
     function openProject(repoName) {
         const githubUrl = `https://github.com/Sun1tAr/${repoName}`;
